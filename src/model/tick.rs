6a9ee6519c5802a7f9a39252cbf1c3a13788700e
Lovish Prajapati <prajapatilovish@gmail.com>
2026-03-02 09:31:24 +0530

    first commit

---
+++ src/model/tick.rs
@@ -0,0 +1,14 @@
+
+use std::time::SystemTime;
+
+#[derive(Debug, Clone)]
+pub struct Tick {
+    pub symbol: String,
+    pub price: f64,
+    pub quantity: f64,
+    pub trade_id: u64,
+    pub side: AggressorSide,
+    pub exchange_ts: u64,
+    pub received_ts: SystemTime
+}
+
